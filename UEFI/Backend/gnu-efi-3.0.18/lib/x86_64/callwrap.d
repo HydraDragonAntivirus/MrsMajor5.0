2a16f3d56511f481dbe2617ee7c250b328ce66f5
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-09-10 16:32:37 +0300

    UEFI

---
+++ UEFI/Backend/gnu-efi-3.0.18/lib/x86_64/callwrap.d
@@ -0,0 +1,2 @@
+C:\Users\victim\Documents\GitHub\MrsMajor5.0\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\callwrap.o: \
+ C:\Users\victim\Documents\GitHub\MrsMajor5.0\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\callwrap.c
---
+++ UEFI/Backend/gnu-efi-3.0.18/lib/x86_64/setjmp.d
@@ -0,0 +1,2 @@
+C:\Users\victim\Documents\GitHub\MrsMajor5.0\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\setjmp.o: \
+ C:\Users\victim\Documents\GitHub\MrsMajor5.0\UEFI\Backend\gnu-efi-3.0.18\lib\x86_64\setjmp.S
